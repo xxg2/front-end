d28c1dfda0c64b971398719f72c2b898d18bf57c
Tom Guan <tom.guan@sap.com>
2014-09-17 16:11:31 +0800

    initial ui5

---
+++ UI5/WebContent/helloworld/index.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("helloworld.index", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf helloworld.index
+	*/ 
+	getControllerName : function() {
+		return "helloworld.index";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf helloworld.index
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
