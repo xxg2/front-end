5e2a7a26730710660f7d6f97c25adecd9f04346b
Tom Guan <tom.guan@sap.com>
2014-09-17 16:15:57 +0800

    initial version

--- sapui5/helloworld/WebContent/getstart/index.controller.js
+++ sapui5/helloworld/WebContent/getstart/index.controller.js
@@ -1,9 +1,9 @@
-sap.ui.controller("helloworld.index", {
+sap.ui.controller("getstart.index", {
 
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf helloworld.index
+* @memberOf getstart.index
 */
 //	onInit: function() {
 //
@@ -12,7 +12,7 @@ sap.ui.controller("helloworld.index", {
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf helloworld.index
+* @memberOf getstart.index
 */
 //	onBeforeRendering: function() {
 //
@@ -21,7 +21,7 @@ sap.ui.controller("helloworld.index", {
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf helloworld.index
+* @memberOf getstart.index
 */
 //	onAfterRendering: function() {
 //
@@ -29,7 +29,7 @@ sap.ui.controller("helloworld.index", {
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf helloworld.index
+* @memberOf getstart.index
 */
 //	onExit: function() {
 //
--- sapui5/helloworld/WebContent/getstart/index.view.js
+++ sapui5/helloworld/WebContent/getstart/index.view.js
@@ -1,16 +1,16 @@
-sap.ui.jsview("helloworld.index", {
+sap.ui.jsview("getstart.index", {
 
 	/** Specifies the Controller belonging to this View. 
 	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-	* @memberOf helloworld.index
+	* @memberOf getstart.index
 	*/ 
 	getControllerName : function() {
-		return "helloworld.index";
+		return "getstart.index";
 	},
 
 	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
 	* Since the Controller is given to this method, its event handlers can be attached right away. 
-	* @memberOf helloworld.index
+	* @memberOf getstart.index
 	*/ 
 	createContent : function(oController) {
  		return new sap.m.Page({
